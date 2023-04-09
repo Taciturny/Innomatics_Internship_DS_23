09487555fedfccfa40dee6848f63e4db0fd4ae79
Uzoagulu Promise C <99205369+Taciturny@users.noreply.github.com>
2023-04-09 12:49:13 +0100

    Update app.py
    app file updated

--- laptop_task/app.py
+++ laptop_task/app.py
@@ -1,61 +1,55 @@
 import streamlit as st
-# import numpy as np
+import numpy as np
 import pandas as pd
 import pickle
 import os
+import joblib
+import json
+
+RESOURCES_PATH = os.path.join(os.getcwd(), 'resource')
+dictPath = os.path.join(RESOURCES_PATH,'laptopdict.json')
+modelPath = os.path.join(RESOURCES_PATH,'model.pkl')
 
 
-RESOURCES_PATH = os.path.join(os.getcwd(), 'resources')
-# Load the trained model
-# model = pickle.load(open('best_model.pkl', 'rb'))
-modelPath = os.path.join(RESOURCES_PATH,'best_model.pkl')
-model = pickle.load(open(modelPath, 'rb'))
+# Load the saved model
+model = joblib.load(modelPath)
 
+# Load the dictionary containing the laptop specifications
+with open(dictPath, 'r') as f:
+    laptopdict = json.load(f)
 
+# Set up the Streamlit app
+st.title('Estimating Laptop Price')
+st.text("""Please enter the specifications of the laptop you're interested in, \n
+and we'll provide an estimated price.""")
 
-
-# Define the input form
-def input_form():
-    ram_size = st.number_input('RAM Size', min_value=1, max_value=2147483647)
-    ram_type = st.number_input('RAM Type', min_value=1, max_value=2147483647)
-    processor = st.number_input('Processor', min_value=1, max_value=2147483647)
-    storage = st.number_input('Storage', min_value=1, max_value=2147483647)
-    os = st.number_input('Operating System', min_value=1, max_value=2147483647)
-    display = st.number_input('Display Size', min_value=1.0, max_value=100.0, step=0.1)
-    storage_type = st.number_input('Storage Type', min_value=1, max_value=2147483647)
-
-    # Store the user input as a dictionary
-    user_input = {'ram_size': ram_size,
-                  'ram_type': ram_type,
-                  'processor': processor,
-                  'storage': storage,
-                  'os': os,
-                  'display': display,
-                  'storage_type': storage_type}
-
-    return user_input
-
-# Define the main function
-def main():
-    # Create a title for the app
-    st.title('Laptop Price Predictor')
-
-    # Create a description for the app
-    st.write('This app predicts the price of a laptop based on its features.')
-
-    # Get user input
-    user_input = input_form()
-
-    # Convert user input to DataFrame
-    input_df = pd.DataFrame([user_input])
+# Create the form for user input
+with st.form('loan_form', clear_on_submit=True):
+    ram_size = st.selectbox('Input Ram Size', laptopdict[0].keys())
+    ram_size = laptopdict[0][ram_size]
     
-    pp = np.array(input_df). reshape (1,-1)
-
-    # Make a prediction using the loaded model
-    prediction = model.predict(pp)
-
-    # Display the predicted price to the user
-    st.write('The predicted price of the laptop is:', prediction[0])
-
-if __name__ == '__main__':
-    main()
+    ram_type = st.selectbox('Input Ram Type', laptopdict[1].keys())
+    ram_type = laptopdict[1][ram_type]
+    
+    processor = st.selectbox('Processor', laptopdict[2].keys())
+    processor = laptopdict[2][processor]
+    
+    storage = st.selectbox('Storage', laptopdict[3].keys())
+    storage = laptopdict[3][storage]
+    
+    storage_type = st.selectbox('Storage Type', laptopdict[4].keys())
+    storage_type = laptopdict[4][storage_type]
+    
+    os_type = st.selectbox('OS', laptopdict[5].values())
+    laptopdict[5] = {value: key for key, value in laptopdict[5].items()}
+    os_type = laptopdict[5][os_type]
+    
+    features = np.array([ram_size, ram_type, processor, storage, storage_type, os_type]).reshape(1,-1)
+    
+    # Add the submit button
+    submitted = st.form_submit_button(label='Submit')
+    
+    if submitted:
+        prediction = round(np.exp(model.predict(features))[0])
+        st.markdown(f'The Estimated Price of the Laptop ₹{prediction}')
+        st.snow()
